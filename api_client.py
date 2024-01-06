c04cb781717836a0037feec25fb70f95f28bd4ec
julotec <135429630+julotec@users.noreply.github.com>
2024-01-06 22:26:47 +0100

    Update api_client.py

--- api_client.py
+++ api_client.py
@@ -4,25 +4,28 @@ import asyncio
 from datetime import datetime, timedelta
 
 
-async def fetch_exchange_rate(session, date): #importuje kursy dla określonej daty
-    url = f"https://api.nbp.pl/api/exchangerates/tables/a/{date}?format=json"
+async def fetch_exchange_rates(session, start_date, end_date):  # importuje kursy dla określonego zakresu dat
+    url = f"https://api.nbp.pl/api/exchangerates/tables/a/{start_date}/{end_date}?format=json"
     async with session.get(url) as response:
-        if response.status == 200:  #sprawdza czy żadanie zakończyło się sukcesem
-            result = await response.json() #odczytuje dane JSON z odpowiedzi i oczekuje że operacja zakończy się
-            return result[0]['rates'] #chcemy uzyskać piewszy element z listy, rates odwołuje się do key rates, który jest pierwszym elementem na liście
+        if response.status == 200:  # sprawdza czy żądanie zakończyło się sukcesem
+            result = await response.json()  # odczytuje dane JSON z odpowiedzi i oczekuje że operacja zakończy się
+            return result[0]['rates']  # chcemy uzyskać pierwszy element z listy, rates odwołuje się do key rates, który jest pierwszym elementem na liście
         else:
-            return None #zwraca None jeśli żądanie się nie powiodło
+            return None  # zwraca None jeśli żądanie się nie powiodło
 
 
 async def main():
-    async with aiohttp.ClientSession() as session: #asynchroniczny kontekst sesji HTTP
-        exchange_rates = [] # lista, która będzie przechowywała kursy wymiany
-        for i in range(10, 0, -1): #pętla iterujaca po ostatich 10 dniach
-            date = (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d')
-            rates = await fetch_exchange_rate(session, date)
-            if rates:
-                exchange_rates.append({date: {'EUR': {'sale': rates[0]['mid'], 'purchase': rates[0]['mid']},
-                                              'USD': {'sale': rates[1]['mid'], 'purchase': rates[1]['mid']}}})
+    async with aiohttp.ClientSession() as session:  # asynchroniczny kontekst sesji HTTP
+        exchange_rates = []  # lista, która będzie przechowywała kursy wymiany
+        end_date = datetime.now().strftime('%Y-%m-%d')
+        start_date = (datetime.now() - timedelta(days=10)).strftime('%Y-%m-%d')
+
+        rates = await fetch_exchange_rates(session, start_date, end_date)
+        if rates:
+            for rate in rates:
+                date = rate['effectiveDate']
+                exchange_rates.append({date: {'EUR': {'sale': rate['mid'], 'purchase': rate['mid']},
+                                              'USD': {'sale': rate['mid'], 'purchase': rate['mid']}}})
         return exchange_rates
 
 
@@ -30,8 +33,8 @@ if __name__ == "__main__":
     if platform.system() == "Windows":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    loop = asyncio.get_event_loop() 
-    result = loop.run_until_complete(main()) 
+    loop = asyncio.get_event_loop()  # pobiera obiekt pętli asynchronicznej
+    result = loop.run_until_complete(main())  # uruchamia funkcję main i czeka na jej zakończenie
     print(result)
 
 
